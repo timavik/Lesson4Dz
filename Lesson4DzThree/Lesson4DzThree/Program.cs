3a85a92a59d81213d713790d9b24fdd0fb136f52
timavik <103438439+timavik@users.noreply.github.com>
2024-12-16 14:37:20 +0300

    Add files via upload

---
+++ Lesson4DzThree/Lesson4DzThree/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Lesson4DzThree
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Последовательность отрицательных чисел:");
+      int i = -20;
+      do
+      {
+        Console.WriteLine(i);
+        i -= 20;
+      }
+      while (i >= -100);
+      Console.WriteLine("\nДля завершения программы нажмите <Enter>");
+      Console.ReadLine();
+    }
+  }
+}
---
+++ Lesson4DzTwo/Lesson4DzTwo/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Lesson4DzTwo
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Двузначные числа, кратные 5: ");
+      for (int i = 10; i < 100; i += 5)
+      {
+        Console.Write($" {i}");
+      }
+      Console.Write(".");
+      Console.WriteLine("\nДля завершения программы нажмите <Enter>");
+      Console.ReadLine();
+    }
+  }
+}
